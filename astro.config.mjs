7b6b06646ca241e07aaa6b0ac727c0526908ccdf
gagoto-dev <gagotodev@gmail.com>
2024-12-30 12:00:24 +0100

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aevoleivallirana.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    // site: 'https://aevoleivallirana.com',
+    integrations: [
+        // sitemap(),
+        tailwind()
+    ],
 });
