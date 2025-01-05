6b50bbb5abc7cd7d62e1a16e367db5d9c4c7c4a6
gagoto-dev <gagotodev@gmail.com>
2025-01-05 12:04:14 +0100

    layout changes

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -29,12 +29,13 @@ export const teams = [
         key: "master-a",
         image: "/master-a.png",
         description: "Equipo mixto de Vallirana",
+        extendedDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         coaches: [findCoach("mel"), findCoach("oscar")],
         players: [
             {
-                name:"Santi",
-                dorsal:1,
-                position:"Central"
+                name: "Santi",
+                dorsal: 1,
+                position: "Central"
             },
             {
                 name: "Alex",
